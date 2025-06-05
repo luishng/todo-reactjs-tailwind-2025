e67baf3afdf9046240446c3e6f2c48e7ae26d1cf
Luis Henrique <luishng@gmail.com>
2025-06-05 11:47:56 -0300

    Create Main Page with Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import PageComponents from "./pages/page-components";
+import LayoutMain from "./pages/layout-main";
+import PageHome from "./pages/page-home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/components" element={<PageComponents />} />
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/components" element={<PageComponents />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
