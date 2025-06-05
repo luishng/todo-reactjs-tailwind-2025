040b2498d4250b1d50330b4a3e6d10e5782614d6
Luis Henrique <luishng@gmail.com>
2025-06-05 15:14:04 -0300

    feat: updating count of tasks in header

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,17 +1,20 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
+  const { createdTasksCount, concluedTasksCount } = useTasks()
+
   return (
     <>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
-        <Badge variant="secondary">5</Badge>
+        <Badge variant="secondary">{createdTasksCount}</Badge>
       </div>
 
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
-        <Badge variant="primary">2 de 5</Badge>
+        <Badge variant="primary">{concluedTasksCount} de {createdTasksCount}</Badge>
       </div>
     </>
   )
